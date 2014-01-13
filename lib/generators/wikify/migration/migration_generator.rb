0110e236185ffa178e0f7365f5d70bc76e9eb039
Adam Laycock <adam@arcath.net>
2014-01-13 17:10:40 +0000

    Chage generator name

--- lib/generators/wikify/migration/migration_generator.rb
+++ lib/generators/wikify/migration/migration_generator.rb
@@ -4,7 +4,7 @@ module Wikify
       argument :model_name, :type => :string, :default => "versions"
       
       def generate_migration
-        generate "migration", "create_#{model_name}", "resource_id:integer", "resource_type", "data:text", "event", "linked_child_resource_type", "linked_child_resource_id:integer", "linked_child:boolean", "comitter_id:integer"
+        generate "migration", "create_wikify_#{model_name}", "resource_id:integer", "resource_type", "data:text", "event", "linked_child_resource_type", "linked_child_resource_id:integer", "linked_child:boolean", "comitter_id:integer"
       end
     end
   end
